4fc7e7d60e8a47f16d37544545fc2f1362589799
Jens Freudenau <jens@php.uk.com>
2011-08-03 15:12:23 +0200

    test mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Trainings1::Application.configure do
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
@@ -29,8 +29,8 @@ Trainings1::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
-
+  #config.serve_static_assets = false
+  config.serve_static_assets = true
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
