d9ffb908c0207786ef9018e75af2201a6f63b079
Jens Freudenau <jens@freude-now.de>
2013-05-03 11:23:54 +0200

    s3 fog
    Signed-off-by: Jens Freudenau <jens@freude-now.de>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIY6YTR5CUMQ2NPEQ',       # required
     :aws_secret_access_key  => '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',       # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :region                 => 'eu-west-1',  # optional, defaults to 'us-east-1'
+    :host                   => 's3-eu-west-1.amazonaws.com',
+    :endpoint               => 'http://s3-eu-west-1.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = 'trainingsdiary' # required
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
