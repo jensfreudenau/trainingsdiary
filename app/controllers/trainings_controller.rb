43dd8519e96fe289f0f48257e1e6eec91c03b693
Jens Freudenau <jens@freude-now.de>
2013-05-03 12:00:42 +0200

    test 2
    Signed-off-by: Jens Freudenau <jens@freude-now.de>

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -247,7 +247,7 @@ class TrainingsController < ApplicationController
     end
 
     def save_file_data(file_data)
-      path = "/trainingsdiary/uploads/training/filename/#{@training.user_id}/#{@training.id}/#{file_data.original_filename.to_s}"
+      path = "uploads/training/filename/#{@training.user_id}/#{@training.id}/#{file_data.original_filename.to_s}"
 
       begin
         td = Trainingsdata::Forerunner.new(path)
