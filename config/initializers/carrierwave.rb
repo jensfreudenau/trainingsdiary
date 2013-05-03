27769a1e5b9ee3e2dbdb64cdcb3f5d8a1f40f7d2
Jens Freudenau <jens@freude-now.de>
2013-05-03 14:03:44 +0200

    eaca
    Signed-off-by: Jens Freudenau <jens@freude-now.de>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,11 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAIY6YTR5CUMQ2NPEQ',       # required
     :aws_secret_access_key  => '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',       # required
     :region                 => 'eu-west-1',  # optional, defaults to 'us-east-1'
-    :host                   => 's3-eu-west-1.amazonaws.com',
-    :endpoint               => 'http://s3-eu-west-1.amazonaws.com' # optional, defaults to nil
+    :host                   => 'http://s3-eu-west-1.amazonaws.com',
   }
   config.fog_directory  = 'trainingsdiary' # required
+  config.fog_public = true
+  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
   # for more optional configuration
 
