a4431fe0fdaafbab625ee267fa55c2a6c033fd6e
Jens Freudenau <jens@freude-now.de>
2013-10-02 10:55:20 +0200

    tabelle anders

--- app/helpers/trainings_helper.rb
+++ app/helpers/trainings_helper.rb
@@ -4,14 +4,36 @@ module TrainingsHelper
     return sprintf('%02d:%02d', res.floor, res*60 % 60);
   end
 
-  def convertTimeToDuration(total_seconds)
+  def convertTimeToDuration(total_seconds, opts = {})
+    opts[:format] ||= :default
+    puts "total: #{total_seconds.to_f}"
+    case opts[:format]
+      when :default
+        seconds = total_seconds % 60
+        minutes = (total_seconds / 60) % 60
+        hours   = total_seconds / (60 * 60)
+        return format("%02d:%02d:%02d", hours, minutes, seconds)
+      when :milliseconds
+        #millis  = total_seconds.ceil
+        puts seconds = total_seconds % 60
+        minutes = (total_seconds / 60) % 60
+        hours   = total_seconds / (60 * 60)
+        puts sec = sprintf("%.2f", seconds)
+        puts sec.to_f
+        return sprintf("%02d:%02d:%02.2f", hours, minutes, sec.to_f)
+    end
+
+
+    #return hours+':'+minutes+':'+minutes
+  end
+
+  def convertTimeToDuration1(total_seconds)
     seconds = total_seconds % 60
     minutes = (total_seconds / 60) % 60
     hours = total_seconds / (60 * 60)
     return format("%02d:%02d:%02d", hours, minutes, seconds)
     #return hours+':'+minutes+':'+minutes
   end
-
 
   def kelvin_to_celsius(kelvin)
     return (kelvin.to_f - 273.16).round(2)
