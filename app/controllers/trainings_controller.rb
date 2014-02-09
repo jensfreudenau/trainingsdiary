f3aaff2c020ad76ecdc8b8efbb4b8bce5de30ded
Jens Freudenau <jens@freude-now.de>
2014-02-09 10:05:43 +0100

    pp1

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -463,25 +463,27 @@ class TrainingsController < ApplicationController
       lat_lon = res[2].split(',')
       time    = DateTime.parse(@training['start_time'].to_s)
       h       = time.strftime("%H")
-      unless lat_lon[0].nil? and !lat_lon[0].is_a? Numeric
-        puts 'huhu'
-        puts  "#{lat_lon[0]},#{lat_lon[1]}"
-        begin
-          weather_data = w_api.history_for(time.strftime("%Y%m%d"), "#{lat_lon[0]},#{lat_lon[1]}")
-          weather_data.each do |data, index|
-            index.each do |v|
-              v.each do |i|
-                if i.class.to_s == 'Array'
-                  i.each do |f|
-                    @log.debug(f['date']['hour'].to_yaml)
-                    if f['date']['hour'].to_s == h.to_s
+      unless lat_lon[0].nil?
+        if lat_lon[0].is_a? Numeric
+          puts 'huhu'
+          puts "#{lat_lon[0]},#{lat_lon[1]}"
+          begin
+            weather_data = w_api.history_for(time.strftime("%Y%m%d"), "#{lat_lon[0]},#{lat_lon[1]}")
+            weather_data.each do |data, index|
+              index.each do |v|
+                v.each do |i|
+                  if i.class.to_s == 'Array'
+                    i.each do |f|
+                      @log.debug(f['date']['hour'].to_yaml)
+                      if f['date']['hour'].to_s == h.to_s
 
-                      @temp     = f['tempm']
-                      @icon     = f['icon']
-                      @weather  = f['conds']
-                      @humidity = f['hum']
-                      @speed    = f['wspdm']
-                      @deg      = f['wdird']
+                        @temp     = f['tempm']
+                        @icon     = f['icon']
+                        @weather  = f['conds']
+                        @humidity = f['hum']
+                        @speed    = f['wspdm']
+                        @deg      = f['wdird']
+                      end
                     end
                   end
                 end
