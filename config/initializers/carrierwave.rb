0a9caa143b0827aa8e8085b4537fdc5d50c00957
Jens Freudenau <jens@freude-now.de>
2018-02-08 09:10:42 +0100

    remove aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAIY6YTR5CUMQ2NPEQ',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => '',       # required
+    :aws_secret_access_key  => '',       # required
     :region                 => 'eu-west-1',  # optional, defaults to 'us-east-1'
     :host                   => 's3-eu-west-1.amazonaws.com',
     #:endpoint               => 'http://s3-eu-west-1.amazonaws.com' # optional, defaults to nil
