1483aa3de8223b39425e34b2bcefd62bc0495996
Jens Freudenau <jens@freude-now.de>
2018-02-08 09:15:33 +0100

    webservice

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => '',       # required
-    :aws_secret_access_key  => '',       # required
+    :aws_access_key_id      => '1234',       # required
+    :aws_secret_access_key  => '123',       # required
     :region                 => 'eu-west-1',  # optional, defaults to 'us-east-1'
     :host                   => 's3-eu-west-1.amazonaws.com',
     #:endpoint               => 'http://s3-eu-west-1.amazonaws.com' # optional, defaults to nil
